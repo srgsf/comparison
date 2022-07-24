d3d2c5754fe1a7d190de7789ef93feead8a99ca0
srgsf <srgsf.github@gmail.com>
2022-07-25 00:19:00 +0300

    go sync with original task.

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+	"runtime"
 	"strconv"
 	"sync"
 	"syscall"
@@ -20,6 +21,7 @@ const (
 	transFile = "translation.txt"
 )
 const freqLimit = 798
+const maxFd uint64 = 65535
 
 var baseFolder = filepath.FromSlash("data/a")
 
@@ -30,21 +32,19 @@ func main() {
 		log.Fatalf("Unable to read directory %s\r\n", baseFolder)
 	}
 	wordsChan := make(chan string)
-	done := make(chan bool, 1)
 	var words []string
 	go func() {
 		for w := range wordsChan {
 			words = append(words, w)
 		}
-		done <- true
 	}()
 
-	var rLimit syscall.Rlimit
-	if err = syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit); err != nil {
+	limit, err := limit()
+	if err != nil {
 		log.Fatal("Unable to read file limits")
 	}
-	guard := make(chan bool, rLimit.Cur)
 
+	guard := make(chan bool, limit)
 	var wg sync.WaitGroup
 	wg.Add(len(files))
 	for _, f := range files {
@@ -54,6 +54,7 @@ func main() {
 			defer func() { <-guard }()
 			freq, err := os.Open(path.Join(baseFolder, f.Name(), freqFile))
 			if err != nil {
+				log.Println(err.Error())
 				return
 			}
 			defer func() {
@@ -69,6 +70,7 @@ func main() {
 			}
 			trans, err := os.Open(path.Join(baseFolder, f.Name(), transFile))
 			if err != nil {
+				log.Println(err.Error())
 				return
 			}
 			defer func() {
@@ -78,14 +80,27 @@ func main() {
 			scanner.Split(bufio.ScanWords)
 			if scanner.Scan() {
 				wordsChan <- scanner.Text()
+				return
 			}
+			wordsChan <- f.Name()
 		}(f)
 	}
 
 	wg.Wait()
 	close(wordsChan)
-	<-done
-	close(done)
+	close(guard)
 	fmt.Printf("selected %d words from %d, took %f seconds\r\n", len(words), len(files),
 		time.Since(start).Seconds())
+}
+
+func limit() (uint64, error) {
+	if runtime.GOOS == "windows" {
+		return maxFd, nil
+	}
+
+	var rLimit syscall.Rlimit
+	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit); err != nil {
+		return 0, err
+	}
+	return rLimit.Cur, nil
 }
